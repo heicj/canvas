0ad9154c16c6a586a72e339247f1cb81d5646846
Charlie <heinercj@gmail.com>
2019-02-21 12:01:31 -0800

    started w3 school canvas walkthrough.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const canvas = document.getElementById("myCanvas")
+// const canvas = document.getElementById("myCanvas")
 
-const ctx = canvas.getContext("2d");
+// const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#FF0000";
+// ctx.fillStyle = "#FF0000";
 
-ctx.fillRect(0, 0, 150, 75);
+// ctx.fillRect(0, 0, 150, 75);
+
+function startGame(){
+  myGameArea.start();
+}
+
+const myGameArea = {
+  canvas: document.getElementById("myCanvas"),
+  start: function(){
+    this.canvas.width = 500,
+    this.canvas.height = 500,
+    this.context = this.canvas.getContext("2d"),
+    document.body.insertBefore(this.canvas, document.body.childNodes[0])
+  }
+}
