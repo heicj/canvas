3b8b1044b06b6422e9db4ccdc7d9f3de6309fa96
Charlie <heinercj@gmail.com>
2019-02-21 14:23:14 -0800

    added interval to redraw game piece

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-// const canvas = document.getElementById("myCanvas")
-
-// const ctx = canvas.getContext("2d");
-
-// ctx.fillStyle = "#FF0000";
-
-// ctx.fillRect(0, 0, 150, 75);
+let myGamePiece;
 
 function startGame(){
   myGameArea.start();
+  myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+function component(width, height, color, x, y){
+  
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.x = x;
+  this.y = y;
+
+  this.update= function(){
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+  }
 }
 
 const myGameArea = {
   canvas: document.getElementById("myCanvas"),
+
   start: function(){
     this.canvas.width = 500,
     this.canvas.height = 500,
     this.context = this.canvas.getContext("2d"),
     document.body.insertBefore(this.canvas, document.body.childNodes[0])
+    this.interval = setInterval(updateGameArea, 20)
+  },
+
+  clear: function(){
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
   }
+}
+
+function updateGameArea(){
+  myGameArea.clear();
+  myGamePiece.update();
 }
